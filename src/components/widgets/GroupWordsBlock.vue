a0863c36442399c319d043a72202c1c7f0d7512c
GeorgyGradinar <W2FbQC6BSedRjdc>
2024-02-08 19:32:24 +0400

    adding dist subtree

---
+++ src/components/widgets/GroupWordsBlock.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>rejfewklfjs</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/widgets/ToggleShowWordsListOrBlock.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ToggleShowWordsListOrBlock"
+}
+</script>
+
+<style scoped>
+
+</style>
